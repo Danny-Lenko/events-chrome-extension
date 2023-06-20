8258331257062020f60029e11d66b5cbaff4364f
Danny-Lenko <devdanny.14@gmail.com>
2023-06-20 17:13:51 +0300

    built microsoft email filter

--- microMailContentScript.js
+++ microMailContentScript.js
@@ -1,5 +1,4 @@
-// let mailNodesFiltered = [];
-const filterString = "filter";
+const filterString = "hello world";
 const reg = new RegExp(filterString, "ig");
 
 // =========================================================== observer
@@ -12,46 +11,24 @@ const observerConfig = {
 };
 
 const observer = new MutationObserver((mutationsList) => {
-  //   const mailNodes =
-  //     document.getElementsByClassName("jEpCF")[0].childNodes[0].childNodes;
+  const mails = document.getElementsByClassName("EeHm8");
 
-//   const mailsContainer = document.getElementsByClassName
+  [...mails].forEach((node) => {
+    const contentNode =
+      node.children.length > 1 ? node.children[1] : node.children[0];
 
-  const mailNodes = [
-    ...document.getElementsByClassName("jEpCF")[0].children[0].children,
-  ];
+    if (contentNode) {
+      const content = contentNode.getAttribute("aria-label");
 
-  const mailNodesFiltered = mailNodes.filter((mail) => {
-    const mailNode = mail.children[0].children[0];
-    //  console.log(content.children[0].children[0].getAttribute("aria-label"));
-    if (mailNode) {
-      // console.log(content.getAttribute("aria-label"));
-      const content = mailNode.getAttribute("aria-label");
-      if (content && !content.match(reg)) {
-        return mail;
+      if (content && content.match(reg)) {
+        node.style.display = "none";
       }
     }
   });
-  console.log(mailNodesFiltered);
-
-
 });
 
 // ======================================================== content script
 
 (() => {
-  //   setTimeout(() => {
-  //     getEvents();
-  //     initialEvents = [...events];
-  //     console.log(initialEvents);
-
-  //     weekDateNode = document.getElementsByClassName("YjxmP");
-  //     weekDateContent = weekDateNode.length && weekDateNode[0].textContent;
-  //   }, initialTimeout);
-
-  //   setTimeout(() => {
-  //     observer.observe(observerNode, observerConfig);
-  //   }, observerTimeout);
-
   observer.observe(observerNode, observerConfig);
 })();
